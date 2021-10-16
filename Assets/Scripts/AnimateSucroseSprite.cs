12fd9310eb3fbd3c364ff44cccd9fe86e2a8a37f
saimachi <saikmachi@gmail.com>
2021-10-16 10:33:07 -0700

    Sucrose model added.
    1. Converted Activation Energy to sprites.
    
    2. Updated animation for sucrose image.

---
+++ Assets/Scripts/AnimateSucroseSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateSucroseSprite : MonoBehaviour
+{
+    private Animator sucroseRotationAnimator;
+
+    #region Private Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        sucroseRotationAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        sucroseRotationAnimator.SetTrigger("Move_Sucrose");
+    }
+    #endregion
+}
---
+++ Assets/Scripts/MoveSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class MoveSprite : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMouseDrag()
+    {
+        rigidBody2D.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    public void OnMouseUp()
+    {
+        rigidBody2D.MovePosition(startPosition);
+    }
+}
