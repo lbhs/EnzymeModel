13b7f90b80e591d9471435400a0bc7efbbd93bac
saimachi <saikmachi@gmail.com>
2021-10-16 11:04:06 -0700

    Additional updates
    1. Scaled saw sprite size down (lower activation energy)
    
    2. Activation energy components added
    
    3. Created fructose and glucose sprites

---
+++ Assets/Scripts/ActivationEnergy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationEnergy : MonoBehaviour
+{
+    [Header("Activation Energy")]
+    public float activationEnergy;
+}
