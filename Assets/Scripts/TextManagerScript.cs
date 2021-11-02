7773d0bf8d34014bbb50d5b13e1f48876d5dea5d
saimachi <saikmachi@gmail.com>
2021-11-01 20:59:44 -0700

    Attempt to fix animations & messages
    Messages added about activation energy & second attempt to fix animations

---
+++ Assets/Scripts/TextManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManagerScript : MonoBehaviour
+{
+    private TMP_Text tmpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpText = GetComponent<TMP_Text>();
+    }
+
+    private void Awake()
+    {
+        EventManager.bondBreakSuccess += DisplaySuccessMessage;
+        EventManager.bondBreakFailure += DisplayFailureMessage;
+    }
+
+    private void DisplaySuccessMessage()
+    {
+        tmpText.text = CommonMessages.successMessage;
+    }
+
+    private void DisplayFailureMessage()
+    {
+        tmpText.text = CommonMessages.failureMessage;
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.bondBreakSuccess -= DisplaySuccessMessage;
+        EventManager.bondBreakFailure -= DisplayFailureMessage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utility Scripts/CommonMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CommonMessages
+{
+    public const string successMessage = "Sufficient activation energy.";
+    public const string failureMessage = "Insufficient activation energy.";
+}
